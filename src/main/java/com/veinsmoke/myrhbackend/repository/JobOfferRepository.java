62d97f26fee22afea86ba5729b7d0bca858956c6
TahaLechgar <tahamr08@gmail.com>
2023-01-16 18:35:09 +0100

    add search by either country or profile (or both) for JobOffer

--- src/main/java/com/veinsmoke/myrhbackend/repository/JobOfferRepository.java
+++ src/main/java/com/veinsmoke/myrhbackend/repository/JobOfferRepository.java
@@ -3,10 +3,20 @@ package com.veinsmoke.myrhbackend.repository;
 import com.veinsmoke.myrhbackend.entity.JobOffer;
 import com.veinsmoke.myrhbackend.entity.projections.JobOfferDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = JobOfferDetail.class)
 public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
+
+    @Query("SELECT j FROM JobOffer j WHERE" +
+            " (:profile IS NULL OR j.profile LIKE %:profile% ) " +
+            "AND (:country IS NULL OR j.country LIKE %:country%)")
+    List<JobOffer> advancedSearch(@Param("profile") String profile,
+                                  @Param("country") String country);
+
 }
 
